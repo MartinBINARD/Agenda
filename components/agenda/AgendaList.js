2eb78da71c52ecf037dbf22a092f50e45cd13ead
MartinBINARD <martin.binard.dev@gmail.com>
2025-01-03 21:56:16 -1000

    Add AgendaList and ListItem components

---
+++ components/agenda/AgendaList.js
@@ -0,0 +1,47 @@
+import { Ionicons } from "@expo/vector-icons";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
+import { colors } from "../../constants/colors";
+
+const Header = () => (
+  <View style={styles.headerContainer}>
+    <Text></Text>
+    <Text style={styles.title}>AGENDA</Text>
+    <Ionicons name="add-circle" size={32} color={colors.PINK} />
+  </View>
+);
+
+export default function AgendaList() {
+  const agendaItems = useSelector((state) => state.agenda.events);
+
+  return (
+    <FlatList
+      data={agendaItems}
+      keyExtractor={({ id }) => id}
+      ItemSeparatorComponent={<View style={{ height: 24 }} />}
+      style={styles.listContainer}
+      renderItem={({ item }) => <ListItem item={item} />}
+      ListHeaderComponent={<Header />}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    paddingHorizontal: 16,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 80,
+    paddingHorizontal: 16,
+    backgroundColor: colors.DARK,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    color: colors.VIOLET,
+  },
+});
