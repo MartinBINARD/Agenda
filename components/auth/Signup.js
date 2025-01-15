c0ee86495b24c6b192ae08ff5f92537a5afa3481
MartinBINARD <martin.binard.dev@gmail.com>
2025-01-14 18:27:40 -1000

    Add authApi reducer

--- components/auth/Signup.js
+++ components/auth/Signup.js
@@ -1,12 +1,16 @@
+import { useSignUpMutation } from '../../store/api/authApi';
 import AuthForm from './AuthForm';
 
 export default function Signup({ navigation }) {
+    const [signUp, { data, isLoading, error }] = useSignUpMutation();
     const navigateToLogin = () => {
         navigation.replace('Login');
     };
     const submitFormHandler = (values) => {
-        console.log(values);
+        signUp({ email: values.email, password: values.password });
     };
 
-    return <AuthForm navigate={navigateToLogin} submitFormHandler={submitFormHandler} isLoading={false} />;
+    console.log(data);
+
+    return <AuthForm navigate={navigateToLogin} submitFormHandler={submitFormHandler} isLoading={isLoading} />;
 }
--- store/store.js
+++ store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { agendaApi } from './api/agendaApi';
+import { authApi } from './api/authApi';
 import agendaReducer from './slices/agendaSlice';
 
 export const store = configureStore({
     reducer: {
         agenda: agendaReducer,
         [agendaApi.reducerPath]: agendaApi.reducer,
+        [authApi.reducerPath]: authApi.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(agendaApi.middleware),
+    middleware: (getDefault) => getDefault().concat(agendaApi.middleware).concat(authApi.middleware),
 });
